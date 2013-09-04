d7ffc522daa37e0bab52d0f8835448ef3416f9ab
Aleksandr Velikonskii <___>
2013-09-04 09:11:41 +0400

    struct nsld

--- include/mat_alg.hpp
+++ include/mat_alg.hpp
@@ -4,42 +4,46 @@
 
 #include <gtest/gtest.h>
 #include  "struct.hpp"
+#include <memory>
 
 namespace velalg {
 
 
-	class algo {
-	public:
-		algo(){};
-		virtual ~algo() {};
+	//class algo {
+	//public:
+		//algo(){};
+		//virtual ~algo() {};
 
-		virtual short div() =0;
-		virtual short mod() =0;
+		//virtual short div() =0;
+		//virtual short mod() =0;
 	
-	};
+	//};
 
-	template <typename T>
-	class dec_alg : public algo {
-	public:
-		dec_alg(){};
-		virtual ~dec_alg() {};
+	//template <typename T>
+	//class dec_alg : public algo {
+	//public:
+		//dec_alg(){};
+		//virtual ~dec_alg() {};
 
-		virtual short div() {return 0;};
-		virtual short mod() {return 0;};
+		//virtual short div() {return 0;};
+		//virtual short mod() {return 0;};
 	
-	};
+	//};
 
 
-	template <typename T>
-	class oct_alg : public algo {
+	//template <typename T>
+	//class oct_alg : public algo {
 
-	public:
-		oct_alg (const T value) {};
-		virtual ~oct_alg() {};
-		virtual short div() {return 0;};
-		virtual short mod() {return 0;};
+	//public:
+		//oct_alg (const T value) {};
+		//virtual ~oct_alg() {};
+		//virtual short div() {return 0;};
+		//virtual short mod() {return 0;};
 	
-	};
+	//};
+
+
+
 
 
 	template <typename T>
@@ -73,13 +77,154 @@ namespace velalg {
 //-------------TEST-----------
 //-------------TEST-----------
 
+//class mat_algTest : public ::testing::Test {
+//protected:
+	//virtual void SetUp() {
+		//al_dec (new velalg::dec_alg<long>() );
+		//al_oct (new velalg::dec_alg<long>() );	
+	//}
+
+//  // virtual void TearDown() {}
+	//std::auto_ptr<velalg::algo> al_dec
+	//std::auto_ptr<velalg::algo> al_dec
+//};
+
+
 TEST( mat_algTest, zeroTest ) {
+
+	std::auto_ptr<velalg::algo> al_dec (new velalg::dec_alg<long>() );
+	velalg::mat_alg<long> ma_dec (0, *(al_dec.get()) );
+	ma_dec.phase();
+	ma_dec.phase();
+	ma_dec.phase();
+
+	EXPECT_EQ( ma_dec.get_unit(), 0);
+	EXPECT_EQ( ma_dec.get_power(), 0);
+
+
+	std::auto_ptr<velalg::algo> al_oct (new velalg::oct_alg<long>() );
+	velalg::mat_alg<long> ma_oct (0, *(al_oct.get()) );
+
+
+	ma_oct.phase();
+	ma_oct.phase();
+	ma_oct.phase();
+
+
+	EXPECT_EQ( ma_oct.get_unit(), 0);
+	EXPECT_EQ( ma_oct.get_power(), 0);
+}
+
+
+TEST( mat_algTest, test1) {
+	std::auto_ptr<velalg::algo> al_dec (new velalg::dec_alg<long>() );
+	std::auto_ptr<velalg::algo> al_oct (new velalg::dec_alg<long>() );
+	velalg::mat_alg<long> ma_dec (1, *(al.get()) );
+	velalg::mat_alg<long> ma_oct (1, *(al.get()) );
+	ma_dec.phase();
+	ma_dec.phase();
+	ma_dec.phase();
+
+
+	ma_oct.phase();
+	ma_oct.phase();
+	ma_oct.phase();
+
+	EXPECT_EQ( ma.get_unit(), 0);
+	EXPECT_EQ( ma.get_power(), 0);
+
+}
+
+
+TEST( mat_algTest, test14) {
 	velalg::algo* al = new velalg::dec_alg<long>();
 	velalg::mat_alg<long> ma (0, *al);
 	ma.phase();
 	EXPECT_EQ( ma.get_unit(), 0);
 	EXPECT_EQ( ma.get_power(), 0);
 }
+
+
+TEST( mat_algTest, test274) {
+	velalg::algo* al = new velalg::dec_alg<long>();
+	velalg::mat_alg<long> ma (0, *al);
+	ma.phase();
+	EXPECT_EQ( ma.get_unit(), 0);
+	EXPECT_EQ( ma.get_power(), 0);
+}
+
+
+TEST( mat_algTest, test4342) {
+	velalg::algo* al = new velalg::dec_alg<long>();
+	velalg::mat_alg<long> ma (0, *al);
+	ma.phase();
+	EXPECT_EQ( ma.get_unit(), 0);
+	EXPECT_EQ( ma.get_power(), 0);
+}
+
+
+TEST( mat_algTest, test43429) {
+	velalg::algo* al = new velalg::dec_alg<long>();
+	velalg::mat_alg<long> ma (0, *al);
+	ma.phase();
+	EXPECT_EQ( ma.get_unit(), 0);
+	EXPECT_EQ( ma.get_power(), 0);
+}
+
+
+TEST( mat_algTest, test434290) {
+	velalg::algo* al = new velalg::dec_alg<long>();
+	velalg::mat_alg<long> ma (0, *al);
+	ma.phase();
+	EXPECT_EQ( ma.get_unit(), 0);
+	EXPECT_EQ( ma.get_power(), 0);
+}
+
+
+TEST( mat_algTest, test4342909) {
+	velalg::algo* al = new velalg::dec_alg<long>();
+	velalg::mat_alg<long> ma (0, *al);
+	ma.phase();
+	EXPECT_EQ( ma.get_unit(), 0);
+	EXPECT_EQ( ma.get_power(), 0);
+}
+
+
+TEST( mat_algTest, test77744455) {
+	velalg::algo* al = new velalg::dec_alg<long>();
+	velalg::mat_alg<long> ma (0, *al);
+	ma.phase();
+	EXPECT_EQ( ma.get_unit(), 0);
+	EXPECT_EQ( ma.get_power(), 0);
+}
+
+
+TEST( mat_algTest, test777444333) {
+	velalg::algo* al = new velalg::dec_alg<long>();
+	velalg::mat_alg<long> ma (0, *al);
+	ma.phase();
+	EXPECT_EQ( ma.get_unit(), 0);
+	EXPECT_EQ( ma.get_power(), 0);
+}
+
+
+TEST( mat_algTest, test4294967295) {
+	velalg::algo* al = new velalg::dec_alg<long>();
+	velalg::mat_alg<long> ma (0, *al);
+	ma.phase();
+	EXPECT_EQ( ma.get_unit(), 0);
+	EXPECT_EQ( ma.get_power(), 0);
+}
+
+
+TEST( mat_algTest, test4294967296) {
+	velalg::algo* al = new velalg::dec_alg<long>();
+	velalg::mat_alg<long> ma (0, *al);
+	ma.phase();
+	EXPECT_EQ( ma.get_unit(), 0);
+	EXPECT_EQ( ma.get_power(), 0);
+}
+
 
 
 #endif//MAT_ALG_HPP

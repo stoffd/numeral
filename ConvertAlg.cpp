a04af2dae8c3ace24b23b9d97e96059845156d6c
Aleksandr Velikonskii <___>
2013-09-06 15:25:32 +0400

    convert ru min tested

--- ConvertAlg.cpp
+++ ConvertAlg.cpp
@@ -31,39 +31,39 @@ namespace velalg {
 		D2String 			d2vMasc, d2vFem;
 		D2String::iterator d2itMasc, d2itFem;
 
-		v1.push_back("1один "); v1.push_back("2два "); v1.push_back("3три ");
-		v1.push_back("4четыре "); v1.push_back("5пять "); v1.push_back("6шесть ");
-		v1.push_back("7семь "); v1.push_back("8восемь "); v1.push_back("9девять "); 
+		v1.push_back("один "); v1.push_back("два "); v1.push_back("три ");
+		v1.push_back("четыре "); v1.push_back("пять "); v1.push_back("шесть ");
+		v1.push_back("семь "); v1.push_back("восемь "); v1.push_back("девять "); 
 		d2vMasc.push_back(v1);
 		v1.clear();
 
 
-		v1.push_back("1aодна "); v1.push_back("2aдве "); v1.push_back("3aтри ");
-		v1.push_back("4aчетыре "); v1.push_back("5aпять "); v1.push_back("6aшесть ");
-		v1.push_back("7aсемь "); v1.push_back("8aвосемь "); v1.push_back("9aдевять "); 
+		v1.push_back("одна "); v1.push_back("две "); v1.push_back("три ");
+		v1.push_back("четыре "); v1.push_back("пять "); v1.push_back("шесть ");
+		v1.push_back("семь "); v1.push_back("восемь "); v1.push_back("девять "); 
 		d2vFem.push_back(v1);
 		v1.clear();
 
 
-		v1.push_back("11одиннадцать "); v1.push_back("12двенадцать "); v1.push_back("13тринадцать ");
-		v1.push_back("14четырнадцать "); v1.push_back("15пятьнадцать "); v1.push_back("16шестнадцать ");
-		v1.push_back("17семнадцать "); v1.push_back("18восемнадцать "); v1.push_back("19девятнадцать "); 
-		d2vMasc.push_back(v1);
-		d2vFem.push_back(v1);
-		v1.clear();
-
-
-		v1.push_back("10десять "); v1.push_back("20двадцать "); v1.push_back("30тридцать ");
-		v1.push_back("40сорок "); v1.push_back("50пятьдесят "); v1.push_back("60шестьдесят ");
-		v1.push_back("70семьдесят "); v1.push_back("80восемьдесят "); v1.push_back("90девяносто "); 
+		v1.push_back("одиннадцать "); v1.push_back("двенадцать "); v1.push_back("тринадцать ");
+		v1.push_back("четырнадцать "); v1.push_back("пятьнадцать "); v1.push_back("шестнадцать ");
+		v1.push_back("семнадцать "); v1.push_back("восемнадцать "); v1.push_back("девятнадцать "); 
 		d2vMasc.push_back(v1);
 		d2vFem.push_back(v1);
 		v1.clear();
 
 
-		v1.push_back("100сто "); v1.push_back("200двести "); v1.push_back("300триста ");
-		v1.push_back("400четыреста "); v1.push_back("500пятьсот "); v1.push_back("600шестьсот ");
-		v1.push_back("700семьсот "); v1.push_back("800восемьсот "); v1.push_back("900девятьсот "); 
+		v1.push_back("десять "); v1.push_back("двадцать "); v1.push_back("тридцать ");
+		v1.push_back("сорок "); v1.push_back("пятьдесят "); v1.push_back("шестьдесят ");
+		v1.push_back("семьдесят "); v1.push_back("восемьдесят "); v1.push_back("девяносто "); 
+		d2vMasc.push_back(v1);
+		d2vFem.push_back(v1);
+		v1.clear();
+
+
+		v1.push_back("сто "); v1.push_back("двести "); v1.push_back("триста ");
+		v1.push_back("четыреста "); v1.push_back("пятьсот "); v1.push_back("шестьсот ");
+		v1.push_back("семьсот "); v1.push_back("восемьсот "); v1.push_back("девятьсот "); 
 		d2vMasc.push_back(v1);
 		d2vFem.push_back(v1);
 		v1.clear();
@@ -85,8 +85,10 @@ namespace velalg {
 
 
 		std::string returnedString ("");
+		
 
-		genders gend = (powerCounter == 1) ? MASCUL : FEM;
+		genders gend = (powerCounter == 1) ? FEM : MASCUL;
+		D2String::iterator vvIt = unList[gend].begin();
 
 		switch (unitCounter) {
 
@@ -95,15 +97,30 @@ namespace velalg {
 			break;
 
 		case 1:
-			if (value == 1)
-				returnedString= *( (( (unList[gend].begin())+1 )->begin())+memCell-1);
-			else if ( (value == 0) && ( memCell != 0 ) )
-				returnedString= *( (( (unList[gend].begin()) )->begin())+memCell-1);
-			else if ( value!=0 )
-				returnedString= *( (( (unList[gend].begin())+2 )->begin())+value-1);
+			if (value == 1) {
+				returnedString = *( ( ( vvIt + 1 )->begin() ) + memCell - 1);
+				memCell = 0;
+			} else if ( (value == 0) && ( memCell != 0 ) ){
+				returnedString = *( ( vvIt->begin() )+memCell - 1 );
+				memCell = 0;
+			} else if ( ( value != 0 ) && ( memCell == 0 ) ) {
+				returnedString = *( ( ( vvIt + 2 )->begin() ) + value - 1 );
+			} else if ( ( value != 0 ) && ( memCell != 0 ) ) {
+				returnedString =  *( ( ( vvIt + 2 )->begin() )+value - 1 );
+				returnedString+=  *( ( vvIt->begin() )+memCell - 1 );
+				memCell = 0;
+			}
 			break;
 
 		case 2:
+			if ( (value == 0) && ( memCell != 0 ) ) {
+				returnedString = *( ( ( vvIt + 2 )->begin() ) + memCell - 1);
+			} else if ( (value == 0) && ( memCell == 0 ) ) {
+				returnedString = "";
+			} else {
+				returnedString = *( ( ( vvIt + 3 )->begin() ) + value - 1);
+			}
+
 			break;
 		
 		default:
@@ -171,42 +188,216 @@ namespace velalg {
 
 
 
+TEST( ConvertAlgTest, minTriplet ) {
 
+	std::string tempStr;
+
+	std::auto_ptr<velalg::ConvertAlg> alRu (new velalg::RusAlg() );
+	velalg::Convert cnvrt (*alRu.get() );
+	
+
+	tempStr = cnvrt.getNumeral(1); 
+	std::cout<<tempStr<<std::endl;
+	EXPECT_STREQ(tempStr.c_str(),"");
+
+	tempStr = cnvrt.getNumeral(1); 
+	std::cout<<tempStr<<std::endl;
+	EXPECT_STREQ( tempStr.c_str() ,"одиннадцать " );
+
+	tempStr = cnvrt.getNumeral(1); 
+	std::cout<<tempStr<<std::endl;
+	EXPECT_STREQ(tempStr.c_str(),"сто ");
+
+}
+
+
+
+
+TEST( ConvertAlgTest, maxTriplet ) {
+
+	std::string tempStr;
+
+	std::auto_ptr<velalg::ConvertAlg> alRu (new velalg::RusAlg() );
+	velalg::Convert cnvrt (*alRu.get() );
+	
+
+	tempStr = cnvrt.getNumeral(9); 
+	std::cout<<tempStr<<std::endl;
+	EXPECT_STREQ(tempStr.c_str(),"");
+
+	tempStr = cnvrt.getNumeral(9); 
+	std::cout<<tempStr<<std::endl;
+	EXPECT_STREQ( tempStr.c_str() ,"девяносто девять " );
+
+	tempStr = cnvrt.getNumeral(9); 
+	std::cout<<tempStr<<std::endl;
+	EXPECT_STREQ(tempStr.c_str(),"девятьсот ");
+
+}
+
+
+
+
+TEST( ConvertAlgTest, single) {
+
+	std::string tempStr;
+
+	std::auto_ptr<velalg::ConvertAlg> alRu (new velalg::RusAlg() );
+	velalg::Convert cnvrt (*alRu.get() );
+	
+
+	tempStr = cnvrt.getNumeral(2); 
+	std::cout<<tempStr<<std::endl;
+	EXPECT_STREQ(tempStr.c_str(),"");
+
+	tempStr = cnvrt.getNumeral(0); 
+	std::cout<<tempStr<<std::endl;
+	EXPECT_STREQ( tempStr.c_str() ,"два " );
+
+	tempStr = cnvrt.getNumeral(0); 
+	std::cout<<tempStr<<std::endl;
+	EXPECT_STREQ(tempStr.c_str(),"");
+
+}
+
+
+TEST( ConvertAlgTest, hundredNdDecs ) {
+
+	std::string tempStr;
+
+	std::auto_ptr<velalg::ConvertAlg> alRu (new velalg::RusAlg() );
+	velalg::Convert cnvrt (*alRu.get() );
+	
+
+	tempStr = cnvrt.getNumeral(0); 
+	std::cout<<tempStr<<std::endl;
+	EXPECT_STREQ(tempStr.c_str(),"");
+
+	tempStr = cnvrt.getNumeral(9); 
+	std::cout<<tempStr<<std::endl;
+	EXPECT_STREQ( tempStr.c_str() ,"девяносто " );
+
+	tempStr = cnvrt.getNumeral(5); 
+	std::cout<<tempStr<<std::endl;
+	EXPECT_STREQ(tempStr.c_str(),"пятьсот ");
+
+
+}
 
 
 
 
 TEST( ConvertAlgTest, xteen) {
 
+	std::string tempStr;
+
 	std::auto_ptr<velalg::ConvertAlg> alRu (new velalg::RusAlg() );
 	velalg::Convert cnvrt (*alRu.get() );
 	
-	EXPECT_STREQ( (cnvrt.getNumeral(7)).c_str(), "" );
-	EXPECT_STREQ( (cnvrt.getNumeral(1)).c_str(), "семнадцать " );
+	tempStr = cnvrt.getNumeral(7); 
+	std::cout<<tempStr<<std::endl;
+	EXPECT_STREQ(tempStr.c_str(),"");
+
+	tempStr = cnvrt.getNumeral(1); 
+	std::cout<<tempStr<<std::endl;
+	EXPECT_STREQ( tempStr.c_str() ,"семнадцать " );
 
 }
 
 
 TEST( ConvertAlgTest, zeroInTheMiddle103) {
 
+	std::string tempStr;
+
 	std::auto_ptr<velalg::ConvertAlg> alRu (new velalg::RusAlg() );
 	velalg::Convert cnvrt (*alRu.get() );
-	
-	EXPECT_STREQ( (cnvrt.getNumeral(3)).c_str(), "" );
-	EXPECT_STREQ( (cnvrt.getNumeral(0)).c_str(), "три " );
-	EXPECT_STREQ( (cnvrt.getNumeral(1)).c_str(), "сто " );
+
+	tempStr = cnvrt.getNumeral(3); 
+	std::cout<<tempStr<<std::endl;
+	EXPECT_STREQ(tempStr.c_str(),"");
+
+	tempStr = cnvrt.getNumeral(0); 
+	std::cout<<tempStr<<std::endl;
+	EXPECT_STREQ( tempStr.c_str() ,"три " );
+
+	tempStr = cnvrt.getNumeral(1); 
+	std::cout<<tempStr<<std::endl;
+	EXPECT_STREQ(tempStr.c_str(),"сто ");
+
 
 }
 
 
 TEST( ConvertAlgTest, fullTriplet) {
 
+	std::string tempStr;
+
 	std::auto_ptr<velalg::ConvertAlg> alRu (new velalg::RusAlg() );
 	velalg::Convert cnvrt (*alRu.get() );
 	
-	EXPECT_STREQ( (cnvrt.getNumeral(3)).c_str(), "три" );
-	EXPECT_STREQ( (cnvrt.getNumeral(5)).c_str(), "пятьдесят " );
-	EXPECT_STREQ( (cnvrt.getNumeral(4)).c_str(), "четыреста " );
+
+	tempStr = cnvrt.getNumeral(4); 
+	std::cout<<tempStr<<std::endl;
+	EXPECT_STREQ(tempStr.c_str(),"");
+
+	tempStr = cnvrt.getNumeral(9); 
+	std::cout<<tempStr<<std::endl;
+	EXPECT_STREQ( tempStr.c_str() ,"девяносто четыре " );
+
+	tempStr = cnvrt.getNumeral(8); 
+	std::cout<<tempStr<<std::endl;
+	EXPECT_STREQ(tempStr.c_str(),"восемьсот ");
 
 }
+
+
+TEST( ConvertAlgTest, twoZeroes300) {
+
+	std::string tempStr;
+
+	std::auto_ptr<velalg::ConvertAlg> alRu (new velalg::RusAlg() );
+	velalg::Convert cnvrt (*alRu.get() );
+	
+
+	tempStr = cnvrt.getNumeral(0); 
+	std::cout<<tempStr<<std::endl;
+	EXPECT_STREQ(tempStr.c_str(),"");
+
+	tempStr = cnvrt.getNumeral(0); 
+	std::cout<<tempStr<<std::endl;
+	EXPECT_STREQ( tempStr.c_str() ,"" );
+
+	tempStr = cnvrt.getNumeral(3); 
+	std::cout<<tempStr<<std::endl;
+	EXPECT_STREQ(tempStr.c_str(),"триста ");
+
+}
+
+
+
+
+
+TEST( ConvertAlgTest, decs) {
+
+	std::string tempStr;
+
+	std::auto_ptr<velalg::ConvertAlg> alRu (new velalg::RusAlg() );
+	velalg::Convert cnvrt (*alRu.get() );
+	
+
+	tempStr = cnvrt.getNumeral(0); 
+	std::cout<<tempStr<<std::endl;
+	EXPECT_STREQ(tempStr.c_str(),"");
+
+	tempStr = cnvrt.getNumeral(7); 
+	std::cout<<tempStr<<std::endl;
+	EXPECT_STREQ( tempStr.c_str() ,"семьдесят " );
+
+	tempStr = cnvrt.getNumeral(0); 
+	std::cout<<tempStr<<std::endl;
+	EXPECT_STREQ(tempStr.c_str(),"");
+
+}
+
+
 
